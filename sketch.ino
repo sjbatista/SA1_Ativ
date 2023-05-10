6c95a6631e9e68a2c829aa3adb399430fe97975e
Salomão Junio Santos Batista <81196440+sjbatista@users.noreply.github.com>
2023-05-10 11:50:10 -0300

    Update sketch.ino
    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -7,8 +7,8 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Inicializando Sistema");
   dhtSensor.setup(13, DHTesp::DHT22);
-  pinMode(22, OUTPUT);
   pinMode(23, OUTPUT);
+  pinMode(18, OUTPUT);
   Serial.println("Finalizando Sistema");
 }
 
@@ -24,9 +24,10 @@ void loop() {
   delay(3000); 
 
   digitalWrite(23, HIGH);
-  digitalWrite(23, HIGH);
+  digitalWrite(18, HIGH);
   delay(1000);
 
   digitalWrite(23, LOW);
-  digitalWrite(23, LOW);
+  digitalWrite(18, LOW);
+  delay(1000);
 }
